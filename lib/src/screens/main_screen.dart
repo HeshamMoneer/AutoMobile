6927cd2b52ceba6685ea8703cffc2fe6c59413b3
Omar Khair <omarkhair66@gmail.com>
2022-12-31 23:53:33 +0200

    fetch all listings from db

--- lib/src/screens/main_screen.dart
+++ lib/src/screens/main_screen.dart
@@ -12,37 +12,50 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   @override
-  Widget build(BuildContext context) {
+  void initState() {
     var allProvider = Provider.of<AllProvider>(context, listen: false);
-    List<Listing> allListings = dummyListings;
+    // should be false to be called from initstate
+    allProvider.fetchListings();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var allProvider = Provider.of<AllProvider>(context, listen: true);
+    List<Listing> allListings = allProvider.getAllListingsAsList();
+    
     return Scaffold(
-      appBar: AppBar(
-        title: Text("AutoMobile"),
-        actions: [
-          IconButton(
-              onPressed: () {
-                showSearch(
-                    context: context, delegate: MySearchDelegate(allListings));
-              },
-              icon: Icon(Icons.search)),
-          IconButton(
-              onPressed: () {
-                allProvider.repository.fireBaseHandler.signout();
-                Navigator.of(context).pushReplacementNamed('/');
-              },
-              icon: Icon(Icons.logout_rounded)),
-        ],
-      ),
-      body: Column(
-        children: [
-          Text("Hello World!!"),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushReplacementNamed('/createListing');
-              },
-              child: Text("Create Listing"))
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("AutoMobile"),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  showSearch(
+                      context: context,
+                      delegate: MySearchDelegate(allListings));
+                },
+                icon: Icon(Icons.search)),
+            IconButton(
+                onPressed: () {
+                  allProvider.repository.fireBaseHandler.signout();
+                  Navigator.of(context).pushReplacementNamed('/');
+                },
+                icon: Icon(Icons.logout_rounded)),
+          ],
+        ),
+        body: RefreshIndicator(
+          onRefresh: allProvider.fetchListings,
+          child: Column(
+            children: [
+              Text("Hello World!!"),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context)
+                        .pushReplacementNamed('/createListing');
+                  },
+                  child: Text("Create Listing"))
+            ],
+          ),
+        ));
   }
 }
